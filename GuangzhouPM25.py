889974e6f0c49c2d2b1aee2cf1c18c0793aa67dd
feipyang <feipyang@gmail.com>
2015-12-15 22:13:30 +0800

    Added more parameters and added all of the map position

--- GuangzhouPM25.py
+++ GuangzhouPM25.py
@@ -38,6 +38,27 @@ DELAY = 15  # secs
 URL = 'http://210.72.1.216:8080/gzaqi_new/RealTimeDate.html'
 CITY = 'guangzhou'
 
+# All Points In Guangzhou City
+#"天河龙洞"
+#"白云山"
+#"麓湖"
+#"公园前"
+#"荔湾西村"
+#"黄沙路边站"
+#"杨箕路边站"
+#"荔湾芳村"
+#"海珠宝岗"
+#"海珠沙园"
+#"海珠湖"
+#"大夫山"
+#"奥体中心"
+#"萝岗西区"
+#"黄埔文冲"
+#"黄埔大沙地"
+#"亚运城"
+#"体育西"
+#"海珠赤沙"
+
 # regex for matching the digits.
 pattern = re.compile(r'\d*')
 floatpattern=re.compile(r'[\d|\.]*')
@@ -64,29 +85,36 @@ def get_air_data():
   # pm2.5 value would be something like xx 微克/立方米, so we need an regex for
   # matching, example: print int(pattern.match(input).group())
   PM25 = int(pattern.match(soup.find('td',{'id': 'pmtow'}).contents[0]).group())
-  print PM25
+  PM25_iaqi = int(pattern.match(soup.find('td',{'id': 'pmtow_iaqi'}).contents[0]).group())
   PM10 = int(pattern.match(soup.find('td',{'id': 'pmten'}).contents[0]).group())
-  print PM10
+  PM10_iaqi = int(pattern.match(soup.find('td',{'id': 'pmten_iaqi'}).contents[0]).group())
   SO2 = int(pattern.match(soup.find('td',{'id': 'sotwo'}).contents[0]).group())
-  print SO2
+  SO2_iaqi = int(pattern.match(soup.find('td',{'id': 'sotwo_iaqi'}).contents[0]).group())
   NO2 = int(pattern.match(soup.find('td',{'id': 'notwo'}).contents[0]).group())
-  print NO2
+  NO2_iaqi = int(pattern.match(soup.find('td',{'id': 'notwo_iaqi'}).contents[0]).group())
+  # Special notice the CO would be float value
   CO = float(floatpattern.match(soup.find('td',{'id': 'co'}).contents[0]).group())
-  print CO
+  CO_iaqi = int(pattern.match(soup.find('td',{'id': 'co_iaqi'}).contents[0]).group())
   O3 = int(pattern.match(soup.find('td',{'id': 'othree'}).contents[0]).group())
-  print O3
+  O3_iaqi = int(pattern.match(soup.find('td',{'id': 'othree_iaqi'}).contents[0]).group())
 
   # Return array contains all of the data
   data = []
   data.append(PM25)
+  data.append(PM25_iaqi)
   data.append(PM10)
+  data.append(PM10_iaqi)
   data.append(SO2)
+  data.append(SO2_iaqi)
   data.append(NO2)
+  data.append(NO2_iaqi)
   data.append(CO)
+  data.append(CO_iaqi)
   data.append(O3)
+  data.append(O3_iaqi)
   return data
   
 if __name__ == '__main__':
   airdata = get_air_data()
   for i in airdata:
-    print i   
+    print i
