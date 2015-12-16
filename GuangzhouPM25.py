aa3eaeb6aac21b6d01b2c46ceb3b741a55956481
purplepalm <purplepalm@gmail.com>
2015-12-16 11:36:19 +0800

    Added more positions and write them into graphite

--- GuangzhouPM25.py
+++ GuangzhouPM25.py
@@ -13,6 +13,9 @@
 # air.city.citypoint.aqi
 # air.city.citypoint.firstp
 # air.city.citypoint.overp
+# When running this script in crontab, be sure to give it a display
+# Example, execute this script every hour at xx:05
+# 5 */1 * * * export DISPLAY=:0;/home/adminubuntu/GuangzhouPM25.py
 ##################################################################################
 
 # BeautifulSoup
@@ -31,10 +34,13 @@ import time
 # Regex
 import re
 
+# pinyin
+import pinyin
+
 # Parameters comes here 
 CARBON_SERVER = '0.0.0.0'
 CARBON_PORT = 2003
-DELAY = 15  # secs
+DELAY = 5  # secs
 URL = 'http://210.72.1.216:8080/gzaqi_new/RealTimeDate.html'
 CITY = 'guangzhou'
 
@@ -54,70 +60,73 @@ def send_msg(message):
   sock.close()
 
 # Fetching data, runs each hour. In one-time access should fetch all of the data. 
-def get_air_data(position):
+def get_air_data(positionsets):
+  # Dictionary hourdata is for holding data, DataStructure like: 
+  # {'baiyunshan': [44, 5], 'haizhubaogang': [55, 6]}
+  hourdata = {}
   # Calling selenium, need linux X
-  with closing(Firefox()) as browser:
-    browser.get(URL)
-    # Now you click corresponding button for get the page refreshed.
-    print position
+  browser = Firefox()
+  browser.get(URL)
+  # Click button one-by-one
+  for position in positionsets:
+    # After clicking, should re-get the page_source.
     browser.find_element_by_id(position).click()
     page_source = browser.page_source
-  # Cooking soup. 
-  soup = BeautifulSoup(page_source, 'html.parser')
-  # pm2.5 value would be something like xx 微克/立方米, so we need an regex for
-  # matching, example: print int(pattern.match(input).group())
-  PM25 = int(pattern.match(soup.find('td',{'id': 'pmtow'}).contents[0]).group())
-  PM25_iaqi = int(pattern.match(soup.find('td',{'id': 'pmtow_iaqi'}).contents[0]).group())
-  PM10 = int(pattern.match(soup.find('td',{'id': 'pmten'}).contents[0]).group())
-  PM10_iaqi = int(pattern.match(soup.find('td',{'id': 'pmten_iaqi'}).contents[0]).group())
-  SO2 = int(pattern.match(soup.find('td',{'id': 'sotwo'}).contents[0]).group())
-  SO2_iaqi = int(pattern.match(soup.find('td',{'id': 'sotwo_iaqi'}).contents[0]).group())
-  NO2 = int(pattern.match(soup.find('td',{'id': 'notwo'}).contents[0]).group())
-  NO2_iaqi = int(pattern.match(soup.find('td',{'id': 'notwo_iaqi'}).contents[0]).group())
-  # Special notice the CO would be float value
-  CO = float(floatpattern.match(soup.find('td',{'id': 'co'}).contents[0]).group())
-  CO_iaqi = int(pattern.match(soup.find('td',{'id': 'co_iaqi'}).contents[0]).group())
-  O3 = int(pattern.match(soup.find('td',{'id': 'othree'}).contents[0]).group())
-  O3_iaqi = int(pattern.match(soup.find('td',{'id': 'othree_iaqi'}).contents[0]).group())
+    # Cooking Soup
+    soup = BeautifulSoup(page_source, 'html.parser')
+    # pm2.5 value would be something like xx 微克/立方米, so we need an regex for
+    # matching, example: print int(pattern.match(input).group())
+    PM25 = int(pattern.match(soup.find('td',{'id': 'pmtow'}).contents[0]).group())
+    PM25_iaqi = int(pattern.match(soup.find('td',{'id': 'pmtow_iaqi'}).contents[0]).group())
+    PM10 = int(pattern.match(soup.find('td',{'id': 'pmten'}).contents[0]).group())
+    PM10_iaqi = int(pattern.match(soup.find('td',{'id': 'pmten_iaqi'}).contents[0]).group())
+    SO2 = int(pattern.match(soup.find('td',{'id': 'sotwo'}).contents[0]).group())
+    SO2_iaqi = int(pattern.match(soup.find('td',{'id': 'sotwo_iaqi'}).contents[0]).group())
+    NO2 = int(pattern.match(soup.find('td',{'id': 'notwo'}).contents[0]).group())
+    NO2_iaqi = int(pattern.match(soup.find('td',{'id': 'notwo_iaqi'}).contents[0]).group())
+    # Special notice the CO would be float value
+    CO = float(floatpattern.match(soup.find('td',{'id': 'co'}).contents[0]).group())
+    CO_iaqi = int(pattern.match(soup.find('td',{'id': 'co_iaqi'}).contents[0]).group())
+    O3 = int(pattern.match(soup.find('td',{'id': 'othree'}).contents[0]).group())
+    O3_iaqi = int(pattern.match(soup.find('td',{'id': 'othree_iaqi'}).contents[0]).group())
+    hourdata_key = pinyin.get(position)
+    hourdata[hourdata_key] = []
+    hourdata[hourdata_key].append(PM25)
+    hourdata[hourdata_key].append(PM25_iaqi)
+    hourdata[hourdata_key].append(PM10)
+    hourdata[hourdata_key].append(PM10_iaqi)
+    hourdata[hourdata_key].append(SO2)
+    hourdata[hourdata_key].append(SO2_iaqi)
+    hourdata[hourdata_key].append(NO2)
+    hourdata[hourdata_key].append(NO2_iaqi)
+    hourdata[hourdata_key].append(CO)
+    hourdata[hourdata_key].append(CO_iaqi)
+    hourdata[hourdata_key].append(O3)
+    hourdata[hourdata_key].append(O3_iaqi)
+  # After clicking all of the button, quit the firefox and return the dictionary
+  browser.close()
+  return hourdata
 
-  # Return array contains all of the data
-  data = []
-  data.append(PM25)
-  data.append(PM25_iaqi)
-  data.append(PM10)
-  data.append(PM10_iaqi)
-  data.append(SO2)
-  data.append(SO2_iaqi)
-  data.append(NO2)
-  data.append(NO2_iaqi)
-  data.append(CO)
-  data.append(CO_iaqi)
-  data.append(O3)
-  data.append(O3_iaqi)
-  return data
-  
 if __name__ == '__main__':
-  airdata = get_air_data("白云山")
+  airdata = get_air_data(positionsets)
   timestamp = int(time.time())
-  # Construct the lines for inserting into the graphite 
-  # example: air.city.citypoint.so2
-  lines = [
-    'air.guangzhou.%s.pm25 %s %d' % ("baiyunmountain", airdata[0], timestamp),
-    'air.guangzhou.%s.pm25_iaqi %s %d' % ("baiyunmountain", airdata[1], timestamp),
-    'air.guangzhou.%s.pm10 %s %d' % ("baiyunmountain", airdata[2], timestamp),
-    'air.guangzhou.%s.pm10_iaqi %s %d' % ("baiyunmountain", airdata[3], timestamp),
-    'air.guangzhou.%s.so2 %s %d' % ("baiyunmountain", airdata[4], timestamp),
-    'air.guangzhou.%s.so2_iaqi %s %d' % ("baiyunmountain", airdata[5], timestamp),
-    'air.guangzhou.%s.no2 %s %d' % ("baiyunmountain", airdata[6], timestamp),
-    'air.guangzhou.%s.no2_iaqi %s %d' % ("baiyunmountain", airdata[7], timestamp),
-    'air.guangzhou.%s.co %s %d' % ("baiyunmountain", airdata[8], timestamp),
-    'air.guangzhou.%s.co_iaqi %s %d' % ("baiyunmountain", airdata[9], timestamp),
-    'air.guangzhou.%s.o3 %s %d' % ("baiyunmountain", airdata[10], timestamp),
-    'air.guangzhou.%s.o3_iaqi %s %d' % ("baiyunmountain", airdata[11], timestamp)
-  ]
-  message = '\n'.join(lines) + '\n'
-  print "######\n"
-  print message
-  print "######\n"
-  for i in airdata:
-    print i
+  for i in airdata.keys():
+    # each key should contains the corresponding hourdata
+    lines = [
+      'air.guangzhou.%s.pm25 %s %d' % (i, airdata[i][0], timestamp),
+      'air.guangzhou.%s.pm25_iaqi %s %d' % (i, airdata[i][1], timestamp),
+      'air.guangzhou.%s.pm10 %s %d' % (i, airdata[i][2], timestamp),
+      'air.guangzhou.%s.pm10_iaqi %s %d' % (i, airdata[i][3], timestamp),
+      'air.guangzhou.%s.so2 %s %d' % (i, airdata[i][4], timestamp),
+      'air.guangzhou.%s.so2_iaqi %s %d' % (i, airdata[i][5], timestamp),
+      'air.guangzhou.%s.no2 %s %d' % (i, airdata[i][6], timestamp),
+      'air.guangzhou.%s.no2_iaqi %s %d' % (i, airdata[i][7], timestamp),
+      'air.guangzhou.%s.co %s %d' % (i, airdata[i][8], timestamp),
+      'air.guangzhou.%s.co_iaqi %s %d' % (i, airdata[i][9], timestamp),
+      'air.guangzhou.%s.o3 %s %d' % (i, airdata[i][10], timestamp),
+      'air.guangzhou.%s.o3_iaqi %s %d' % (i, airdata[i][11], timestamp)
+    ]
+    message = '\n'.join(lines) + '\n'
+    send_msg(message)
+    # delay for graphite server will use a DELAY time for inserting data
+    time.sleep(DELAY)
